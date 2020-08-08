5e04446e7d6457d7f81bbf65f388faf028c97e98
johnnyflame <johnnyflamelee@gmail.com>
2020-08-08 15:21:32 +1200

    added word solver

--- constraint_satisfaction/word_search.py
+++ constraint_satisfaction/word_search.py
@@ -12,7 +12,7 @@ class GridLocation(NamedTuple):
     column: int
 
 
-def generate_grid(row: int, columns: int) -> Grid:
+def generate_grid(rows: int, columns: int) -> Grid:
     return [[choice(ascii_uppercase) for c in range(columns)] for r in range(rows)]
 
 
@@ -20,3 +20,60 @@ def display_grid(grid: Grid) -> None:
     for row in grid:
         print("".join(row))
 
+
+def generate_domain(word: str, grid: Grid) -> List[List[List[GridLocation]]]:
+    domain: List[List[GridLocation]] = []
+    height: int = len(grid)
+    width: int = len(grid[0])
+    length: int = len(word)
+    for row in range(height):
+        for col in range(width):
+            columns: range = range(col, col + length + 1)
+            rows: range = range(row, row + length + 1)
+            if col + length <= width:
+                # left to right
+                domain.append([GridLocation(row, c) for c in columns])
+                # diagonal towards bottom right
+                if row + length <= height:
+                    domain.append([GridLocation(r, col + (r - row)) for r in rows])
+            if row + length <= height:
+                # top to bottom
+                domain.append([GridLocation(r, col) for r in rows])
+                # diagonal towards bottom left
+                if col - length >= 0:
+                    domain.append([GridLocation(r, col - (r - row)) for r in rows])
+    return domain
+
+
+class WordSearchConstraint(Constraint[str, List[GridLocation]]):
+    def __init__(self, words: List[str]) -> None:
+        super().__init__(words)
+        self.words: List[str] = words
+
+    def satisfied(self, assignment: Dict[str, List[GridLocation]]) -> bool:
+        # if there is any duplicates grid locations, then there is an overlap
+        all_locations = [locs for values in assignment.values() for locs in values]
+        return len(set(all_locations)) == len(all_locations)
+
+
+if __name__ == "__main__":
+    grid: Grid = generate_grid(9, 9)
+    words: List[str] = ["MATTHEW", "JOE", "MARY", "SARAH", "SALLY"]
+    locations: Dict[str, List[GridLocation]] = {}
+    for word in words:
+        locations[word] = generate_domain(word, grid)
+    csp: CSP[str, List[GridLocation]] = CSP(words, locations)
+    csp.add_constraint(WordSearchConstraint(words))
+    solution: Optional[Dict[str, List[GridLocation]]] = csp.backtracking_search()
+
+    if solution is None:
+        print("no solution found")
+    else:
+        for word, grid_locations in solution.items():
+            # random reverse half the time
+            if choice([True, False]):
+                grid_locations.reverse()
+            for index, letter in enumerate(word):
+                (row, col) = (grid_locations[index].row, grid_locations[index].column)
+                grid[row][col] = letter
+        display_grid(grid)
